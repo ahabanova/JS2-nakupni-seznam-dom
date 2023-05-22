864952825d1e0c6cd9ebea92c402c656710b822d
ahabanova <ahabanova95@gmail.com>
2023-05-22 17:19:02 +0200

    hotovo z lekce 5

--- ListItem/index.js
+++ ListItem/index.js
@@ -1,21 +1,30 @@
-//komponenta (lepší pojmenovat s velkým písmenem, dělá se tam v reactu)
-//props je vždy objekt s nějakými vlastnostmi
-
 export const ListItem = (props) => {
   const { product, amount, unit, done } = props
+
+  const element = document.createElement('div')
+  element.classList.add('list-item')
 
   let tickClass = '';
   if (done) {
     tickClass = ' btn-tick--on';
   }
 
-  return `
-      <div class="list-item">
-        <button class="icon-btn btn-tick${tickClass}"></button>
-        <div class="list-item__body">
-          <div class="list-item__product">${product}</div>
-          <div class="list-item__amount">${amount} ${unit}</div>
-        </div>
-      </div>
-    `
+  element.innerHTML = `
+  <button class="icon-btn btn-tick${tickClass}"></button>
+  <div class="list-item__body">
+  <div class="list-item__product">${product}</div>
+  <div class="list-item__amount">${amount} ${unit}</div>
+`
+
+  const btnElm = element.querySelector('button')
+  btnElm.addEventListener('click', () => {
+    element.replaceWith(ListItem({
+      product: product,
+      amount: amount,
+      unit: unit,
+      done: !done,
+    }))
+  })
+
+  return element
 }
--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const mainElm = document.querySelector('main')
 fetch('https://nakupy.kodim.app/api/sampleweek/mon')
   .then((response) => response.json())
   .then((data) => {
-    mainElm.innerHTML += ShopList({
+    mainElm.append(ShopList({
       dayName: data.result.dayName,
       items: data.result.items
-    })
+    }))
   });
 
 fetch('https://nakupy.kodim.app/api/sampleweek/tue')
   .then((response) => response.json())
   .then((data) => {
-    mainElm.innerHTML += ShopList({
+    mainElm.append(ShopList({
       dayName: data.result.dayName,
       items: data.result.items
-    })
+    }))
   });
